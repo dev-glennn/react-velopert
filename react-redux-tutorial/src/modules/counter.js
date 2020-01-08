94b3a1372103d3c54c745bc431620e8e88c8a19f
dev-glennn <dev.glennn@gmail.com>
2020-01-08 10:17:26 +0900

    리덕스 더 편하게 사용하기
    1. redux-actions(createAction,handleActions)
    2. immer(produce)

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,29 +1,24 @@
+import { createAction, handleActions } from 'redux-actions'
+
 //모듈이름/
 const INCREASE = 'counter/INCREASE'
 const DECREASE = 'counter/DECREASE'
 
 //액션 생성 함수 만들기
-export const increase = () => ({ type: INCREASE })
-export const decrease = () => ({ type: DECREASE })
+export const increase = createAction(INCREASE)
+export const decrease = createAction(DECREASE)
 
 const initialState = {
   number: 0,
 }
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      }
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      }
-    default:
-      return state
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+)
 
 //export 는 여러개, export default 는 단 한개만 내보낼 수 있음
 //export 는 {} 안에 export default 는 {} 없이 불러올 수 있음
