538809dff4923ccad2e91ed0fb85f9a36589794f
dev-glennn <dev.glennn@gmail.com>
2020-01-08 10:59:52 +0900

    Hooks를 사용하여 컨테이너 컴포넌트 만들기
    1. useSelector로 상태 조회하기
    2. useDispatch를 사용하여 액션 디스패치하기
    3. useStore를 사용하여 리덕스 스토어 사용하기
    4. TodosContainer를 Hooks로 전환하기
    5.useActions 유틸 Hook을 만들어서 사용하기

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,34 +1,17 @@
-import React from 'react'
-//컴포넌트에서 액션을 디스패치하기 위해 각 액션 생성 함수를 호출하고 dispatch로 감싸는 작업을 간단히 할 수 있는 유틸 함수
-// import { bindActionCreators } from 'redux'
-//컴포넌트와 리덕스를 연동하기 위한 함수
-//connect(리덕스 스토어 안의 상태를 props로 넘겨주는 함수, 액션 생성 함수를 props로 넘겨주는 함수)
-import { connect } from 'react-redux'
+import React, { useCallback } from 'react'
+//connect 대신 useSelector 사용
+import { useSelector, useDispatch } from 'react-redux'
 import Counter from '../components/Counter'
 import { increase, decrease } from '../modules/counter'
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number)
+  const dispatch = useDispatch()
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch])
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch])
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  )
 }
 
-// const mapStateToProps = state => ({
-//   number: state.counter.number,
-// })
-
-// const mapDispatchToProps = dispatch => ({
-//   increase: () => {
-//     dispatch(increase())
-//   },
-//   decrease: () => {
-//     dispatch(decrease())
-//   },
-// })
-
-export default connect(
-  state => ({
-    number: state.counter.number,
-  }),
-  //bindActionCreators 내에 담은 함수가 객체 형태로 넘어감, 선언한 뒤 호출⭐
-  //   dispatch => bindActionCreators({ increase, decrease }, dispatch),
-  { increase, decrease },
-)(CounterContainer)
+export default CounterContainer
