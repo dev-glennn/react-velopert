e0b83d4ea3ff190b4ec13694df2141b57986ce41
dev-glennn <dev.glennn@gmail.com>
2020-01-08 18:00:50 +0900

    비동기 작업을 처리하는 미들웨어 사용
    p494

--- learn-redux-middleware/src/containers/CounterContainer.js
+++ learn-redux-middleware/src/containers/CounterContainer.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { increase, decrease } from '../moduls/counter'
+import { increaseAsync, decreaseAsync } from '../moduls/counter'
 import Counter from '../components/Counter'
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+const CounterContainer = ({ number, increaseAsync, decreaseAsync }) => {
+  return <Counter number={number} onIncrease={increaseAsync} onDecrease={decreaseAsync} />
 }
 
 export default connect(
   state => ({
     number: state.counter,
   }),
-  { increase, decrease },
+  { increaseAsync, decreaseAsync },
 )(CounterContainer)
