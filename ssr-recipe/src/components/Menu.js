659e856f8a1c6926d57953b4a55dbfee3ebf6640
dev-glennn <dev.glennn@gmail.com>
2020-01-14 13:46:05 +0900

    yarn eject 전 commit

--- ssr-recipe/src/components/Menu.js
+++ ssr-recipe/src/components/Menu.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Menu = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  return (
+    <ul>
+      <li>
+        <Link to="/red">Red</Link>
+      </li>
+      <li>
+        <Link to="/blue">Blue</Link>
+      </li>
+    </ul>
+  )
+}
 
-export default Menu;
+export default Menu
